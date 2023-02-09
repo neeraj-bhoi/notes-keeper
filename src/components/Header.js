0af3f4fd69b47e0c401799a510ac769756d8d3d4
neeraj-bhoi <neerajbhoi2001@gmail.com>
2023-02-09 21:59:54 +0530

    header updated

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,9 +3,6 @@ import React from "react";
 function Header() {
   return (
     <header>
-      {/* <div className="bulbimage">
-        <img src="https://img.icons8.com/dotty/100/null/light-on.png" alt="bulbimage"/> {" "}
-      </div> */}
       <h1> Notes-Keeper </h1>
     </header>
   );
