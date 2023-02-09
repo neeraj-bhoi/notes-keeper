29ba4590bba00739f2bb8a323983441f744fab74
neeraj-bhoi <neerajbhoi2001@gmail.com>
2023-02-09 21:57:36 +0530

    header and components updated

---
+++ src/components/App.js
@@ -0,0 +1,15 @@
+import React, { useState } from "react";
+import CreateArea from "./CreateArea";
+import Footer from "./Footer";
+import Header from "./Header";
+import Note from "./Note";
+
+function App() {
+  return (
+    <div>
+      <Header />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Header() {
+  return (
+    <header>
+      {/* <div className="bulbimage">
+        <img src="https://img.icons8.com/dotty/100/null/light-on.png" alt="bulbimage"/> {" "}
+      </div> */}
+      <h1> Notes-Keeper </h1>
+    </header>
+  );
+}
+export default Header;
